6e5a4961f7e571a27d4bb80e4e04435dff7b44fa
dianaperez25 <52755120+dianaperez25@users.noreply.github.com>
2020-05-22 10:28:13 -0500

    Deleted extra maketaskdconns script, and made the finalized version the main/only one

--- MakeTaskDconns.m
+++ MakeTaskDconns.m
@@ -1,7 +1,6 @@
-% This script makes Dconns (and saves them if the MakeDconn variable = 1) and variant files
+% This script makes Dconns (if the MakeDconn variable = 1) and variant files
 % (if VariantMap = 1)for task data of each subject's even and odd sessions. 
-% QC files need to be named [subject]_QCFile.mat, ex. 'MSC01_QCFile.mat'
-% Dependecies: MSC Data, 
+% QC files need to be na med [subject]_QCFile.mat, ex. 'MSC01_QCFile.mat'
 % Written by Brian Kraus, edited by Diana Perez.
 
 %parpool('local', 28)     %% Name of cluster profile for batch job (how many workers/cores you need to run this job)
@@ -34,84 +33,10 @@ SaveTimeseries = 0;     %% Save concatenated timeseries for subject
 subs = {'MSC01','MSC02','MSC03','MSC04','MSC05','MSC06','MSC07','MSC09','MSC10'};
 tasks = {'motor','mem','mixed'};
 voxnum = 59412; % number of voxels for template
-outtxtfile = [];
-
-% sets up variables for sum of sample points in each task of even- and odd-numbered
-% sessions to be used later
-memptsoddsum = [];
-motorptsoddsum = [];
-mixedptsoddsum = [];
-memptsevensum = [];
-motorptsevensum = [];
-mixedptsevensum = [];
 %% Start analysis
 disp(sprintf('Job Started: %s', datestr(now)));
 %% Matches data points for all tasks across subjects
-%[mintasksamps] = mintasksamps(QCFiles_path, subs, SplitHalf, ConcatenateTasks, MatchData, MatchAcrossSubs)
-if MatchData == 1 && MatchAcrossSubs == 1
-% This for-loop determines the number of sample points to match
-    for n=1:numel(subs)
-        
-        load ([QCFiles_path subs{n} '_QCFile.mat']); 
-        
-        % sets up variables for number of sample points available for
-        % current subject
-        memptsodd = [];
-        motorptsodd = [];
-        mixedptsodd = [];
-        memptseven = [];
-        motorptseven = [];
-        mixedptseven = [];
-        
-         % QC files contain SubStruct with # of sample points for each task
-         % and whether the session is an even- or odd-numbered session
-        for u = 1:length(SubStruct)
-            
-            % divides sample points between even and odd sessions
-            % 1 = Odd Session, 2 = Even Session
-            if SubStruct(u).OddEven == 1
-                memptsodd = [memptsodd; SubStruct(u).MemSampPts];
-                mixedptsodd = [mixedptsodd; SubStruct(u).MixedSampPts];
-                if ~strcmp(subs{n}, 'MSC09') % different for MSC09 because not enough motor sample points
-                    motorptsodd = [motorptsodd; SubStruct(u).MotorSampPts];
-                end
-            elseif SubStruct(u).OddEven == 2
-                memptseven = [memptseven; SubStruct(u).MemSampPts];
-                mixedptseven = [mixedptseven; SubStruct(u).MixedSampPts];
-                if ~strcmp(subs{n}, 'MSC09')
-                    motorptseven = [motorptseven; SubStruct(u).MotorSampPts];
-                end
-      
-            end
-        end
-    
-        memptsoddsum = [memptsoddsum; sum(memptsodd)];
-        mixedptsoddsum = [mixedptsoddsum; sum(mixedptsodd)];
-        memptsevensum = [memptsevensum; sum(memptseven)];
-        mixedptsevensum = [mixedptsevensum; sum(mixedptseven)];
-        
-        % sums all sample points for all even- or odd-numbered sessions for
-        % current subject
-        if strcmp(subs{n}, 'MSC09')
-            motorptsoddsum = [motorptsoddsum; 9999];
-            motorptsevensum = [motorptsevensum; 9999];
-        else
-            motorptsoddsum = [motorptsoddsum; sum(motorptsodd)];
-            motorptsevensum = [motorptsevensum; sum(motorptseven)];
-        end
-
-        
-    end
-    
-    %finds minimum number of sample points to match the other tasks across
-    %subjects
-    if SplitHalf == 1
-        mintasksamps = min(min([memptsoddsum motorptsoddsum mixedptsoddsum memptsevensum motorptsevensum mixedptsevensum]));
-    elseif ConcatenateTasks == 1
-        mintasksamps = min(min([(memptsoddsum + memptsevensum)  (motorptsoddsum + motorptsevensum)  (mixedptsoddsum + mixedptsevensum)]));
-    end    
-end
-
+[mintasksamps] = mintasksamps(QCFiles_path, subs, SplitHalf, ConcatenateTasks, MatchData, MatchAcrossSubs)
 %% Main for-loop: makes Dconns
 for i=1:numel(subs)
     
